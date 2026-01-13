72fa03aedb84aa7dd2ae2baa942aeeacc09ff92d
BigBoPeep420 <236509553+BigBoPeep420@users.noreply.github.com>
2026-01-12 23:19:04 -0600

    Update JS, html, css
    Finished JS functionality to add and remove books,
        and a book info dialog.
    Did some css styling, needs more (and a theme).
    Added some html to support popovers and tooltip.

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,52 @@
 import { Book } from "./modules/book.module.js";
-import { addToLibrary, lib } from "./modules/library.module.js";
+import { Library } from "./modules/library.module.js";
+
+const library = new Library();
+const addBookDialog = document.getElementById('dialogAddBook');
+const bookInfoDialog = document.getElementById('dialogBookInfo');
+
+addBookDialog.querySelector('form').addEventListener('submit', e => {
+    const formData = new FormData(e.target);
+    if(!e.target.checkValidity()){
+        e.target.preventDefault();
+        e.target.reportValidity();
+    }else{
+        const data = Object.fromEntries(formData.entries());
+        const book = new Book(data['title'], data['author'], data['pages'], 
+            data['description'], data['read']);
+        makeBookCard(book);
+    }
+})
+document.getElementById('addBook').addEventListener('click', e => {
+    showAddBook(true);
+})
+bookInfoDialog.querySelector('.read').addEventListener('mouseenter', e => {
+    const read = bookInfoDialog.querySelector('.read').dataset.read;
+    let readTxt = '';
+    switch(read){
+        case 'yes':
+            readTxt = 'been read';
+            break;
+        case 'no':
+            readTxt = 'not been read';
+            break;
+        case 'started':
+            readTxt = 'been started';
+            break;
+    }
+    bookInfoDialog.querySelector('.tooltip').textContent = `This book has ${readTxt}!`;
+})
+
+
+
+library.addToLibrary('jrt', 'hob', 23, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est saepe consequatur reasdasdfsdgasd', 'yes');
+fillBookCards();
+showBookInfo(true, library.lib[0]);
 
 function makeBookCard(book){
     const card = document.createElement('div');
     card.classList.add('bookCard');
     card.dataset.bookId = book.id;
-    
     for (let attr in book){
         if(attr == 'id'){
             card.dataset.bookId = book.id
@@ -17,33 +58,64 @@ function makeBookCard(book){
                 const pg = document.createElement('span');
                 pg.textContent = 'pg';
                 elem.append(pg);
-            }else if(attr == 'read'){
-                const rd = document.createElement('p');
-                rd.classList.add('read');
-                card.dataset.read = `${book[attr]}`;
-                rd.textContent = '⏺';
-                elem.append(rd)
-            }else {
+            }else if(attr != 'read') {
                 elem.textContent = `${book[attr]}`;
             }
             card.append(elem);
         }
     }
+    const rd = document.createElement('p');
+    rd.classList.add('read');
+    card.dataset.read = `${book.read}`;
+    rd.textContent = '⏺';
+    card.append(rd)
+    const del = document.createElement('button');
+    del.classList.add('delete');
+    del.textContent = '❌';
+    del.addEventListener('click', e => {
+        library.removeFromLibrary(e.target.closest('.bookCard').dataset.bookId);
+        fillBookCards();
+    })
+    card.append(del);
     document.getElementById('books').append(card);
+    card.addEventListener('click', e => {
+        e.preventDefault();
+        const target = e.target.closest('.bookCard');
+        
+        showBookInfo(true, library.getBook(target.dataset.bookId));
+    })
 }
 function fillBookCards(){
-    for(const book in lib){
-        makeBookCard(lib[book]);
+    const bookCards = document.getElementById('books').querySelectorAll('.bookCard');
+    bookCards.forEach(element => {
+        element.remove();
+    });
+    for(const book in library.lib){
+        makeBookCard(library.lib[book]);
     }
 }
-
-const jrt = addToLibrary('The Hobbit', 'JRR Tolkein', 300, 
-    'A story about a Hobbit who leaves the comfort of his Shire behind to save the world ' +
-    'and make new friends along the way.', 'yes');
-fillBookCards();
-
-
-const addBookDiag = document.getElementById('dialogAddBook');
-addBookDiag.inert = true;
-addBookDiag.showModal();
-addBookDiag.inert = false;
+function showAddBook(bool){
+    if(bool === true){
+        addBookDialog.inert = true;
+        addBookDialog.querySelector('form').reset();
+        addBookDialog.showModal();
+        addBookDialog.inert = false;
+    }else addBookDialog.close();
+}
+function showBookInfo(bool, book){
+    if(bool && book){
+        bookInfoDialog.inert = true;
+        bookInfoDialog.querySelector('.title').textContent = book.title;
+        bookInfoDialog.querySelector('.author').textContent = book.author;
+        bookInfoDialog.querySelector('.pages').textContent = book.pages;
+        const pg = document.createElement('span');
+        pg.textContent = 'pg';
+        bookInfoDialog.querySelector('.pages').appendChild(pg);
+        bookInfoDialog.querySelector('.description').textContent = book.description;
+        bookInfoDialog.querySelector('.read').dataset.read = book.read;
+        bookInfoDialog.showModal();
+        bookInfoDialog.inert = false;
+    }else if(bool == false){
+        bookInfoDialog.close();
+    }
+}
--- scripts/modules/library.module.js
+++ scripts/modules/library.module.js
@@ -1,9 +1,24 @@
 import { Book } from './book.module.js'
 
-let lib = [];
-
-function addToLibrary(tit, auth, pgs, desc, rd){
-    lib.push(new Book(tit, auth, pgs, desc, rd));
+function Library(){
+    this.lib = [];
+    this.addToLibrary = function(tit, auth, pgs, desc, rd){
+        this.lib.push(new Book(tit, auth, pgs, desc, rd));
+    }
+    this.removeFromLibrary = function(id){
+        const index = this.lib.findIndex(book => book.id === id);
+        if(index > -1){
+            this.lib.splice(index, 1);
+        }
+    }
+    this.getBook = function(id){
+        const index = this.lib.findIndex(book => book.id === id)
+        if(index > -1){
+            return this.lib[index];
+        }else{
+            console.error('Book not found in this.lib');
+        }
+    }
 }
 
-export { addToLibrary, lib };
+export { Library };
